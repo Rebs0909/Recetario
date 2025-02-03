6a545ca2ee98ab5227220a9879b8d6374aa7960c
Rebeca Palma <rebeca.palma.diaz2@gmail.com>
2025-02-03 14:10:09 -0600

    App is functional now

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ let formValidation = () => {
   } else {
     console.log("success");
     msg.innerHTML = "";
+    acceptData();
   }
 };
 //this block accepts the data ** it-s incomplete!!**
@@ -28,4 +29,28 @@ let data = {};
 let acceptData = () => {
   data["text"] = input.value;
   console.log(data);
+  createPost();
+};
+
+let createPost = () => {
+  posts.innerHTML += `
+  <div>
+    <p>${data.text}</p>
+    <span class="options">
+      <i onClick="editPost(this)" class="fas fa-edit"></i>
+      <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+    </span>
+  </div>
+  `;
+  input.value = "";
+};
+
+let deletePost = (e) => {
+  e.parentElement.parentElement.remove();
+};
+`<i onClick="deletePost(this)" class="fas fa-trash-alt"></i>`;
+
+let editPost = (e) => {
+  input.value = e.parentElement.previousElementSibling.innerHTML;
+  e.parentElement.parentElement.remove();
 };
