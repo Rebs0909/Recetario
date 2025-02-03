e8089c77937e61289deacc00fec6545e69dce2e2
Rebeca Palma <rebeca.palma.diaz2@gmail.com>
2025-02-03 01:30:02 -0600

    linked the js file and commented some lines so I don't forget their function.

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
-let form = document.getElementById("form");
-let input = document.getElementByuId("msg");
+let form = document.getElementById("form"); //this links the form variable to the form element from my Html file, and the next lines of code do the same for a different element
+let input = document.getElementById("input");
 let msg = document.getElementById("msg");
 let posts = document.getElementById("posts");
+
+//this listens for a submit event on the form element.
+form.addEventListener("submit", (e) => {
+  //prevents the default form submission, which would reload the page
+  e.preventDefault();
+  console.log("button clicked");
+
+  //this block is a function that checks if the imput field(input.value) is empty to-
+  //determine if it should print failure or success
+  formValidation();
+});
+let formValidation = () => {
+  if (input.value === "") {
+    msg.innerHTML = "Post cannot be blank";
+    console.log("failure");
+  } else {
+    console.log("success");
+    msg.innerHTML = "";
+  }
+};
+//this block accepts the data ** it-s incomplete!!**
+let data = {};
+
+let acceptData = () => {
+  data["text"] = input.value;
+  console.log(data);
+};
